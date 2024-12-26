3198cc81822791b81fd24f7c0ef0ed6ab87597fb
Jonathanrazakalalaina <jonathanbill17@gmail.com>
2024-12-26 15:10:55 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://f3cdedd9917f8205a9d0eb4250749008@o4508493595344896.ingest.us.sentry.io/4508493597507584",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
